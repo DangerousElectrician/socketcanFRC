4c677a4348d685a7e7fc25825186e822d251b7e0
Lou <DangerousElectrician@gmail.com>
2017-01-26 23:52:20 -0600

    stream sessions might work now

--- CANSessionMux.cpp
+++ CANSessionMux.cpp
@@ -34,7 +34,7 @@ extern "C"
 	struct sockaddr_can addr;
 	int s;
 	int sbcm;
-	struct can_filter rfilter[1];
+	//struct can_filter rfilter[1];
 
 	struct can_msg {
 		struct bcm_msg_head msg_head;
@@ -91,7 +91,6 @@ extern "C"
 	}
 // this function does not block
 // proper behavior is to return the last received message with requested messageID
-// TODO: have old messages expire
 	struct timeval last_tv;
 	void FRC_NetworkCommunication_CANSessionMux_receiveMessage(uint32_t *messageID, uint32_t messageIDMask, uint8_t *data, uint8_t *dataSize, uint32_t *timeStamp, int32_t *status) {
 
@@ -150,27 +149,44 @@ extern "C"
 
 //sessionHandle: set this integer to identify a session
 //messageID: arbID of messages to cache
-//messageIDMask: CanTalonSRX sets this to 0xFFFFFFFF, probably can ignore
-//maxMessages: number of messages to cache
+//messageIDMask: CanTalonSRX sets this to 0xFFFFFFFF
+//maxMessages: maximum number of messages that can be returned at once, CanTalonSRX has this set to 20
 //status: set to zero if everything worked
 	uint32_t _messageID = 0;
 	void FRC_NetworkCommunication_CANSessionMux_openStreamSession(uint32_t *sessionHandle, uint32_t messageID, uint32_t messageIDMask, uint32_t maxMessages, int32_t *status) {
-		*sessionHandle = 1; //set the session handle to something or else CanTalonSRX won't continue
-		_messageID = messageID;
+		*sessionHandle = socket(PF_CAN, SOCK_RAW | SOCK_NONBLOCK, CAN_RAW); //create nonblocking raw can socket
+		struct can_filter rfilter[1];
+		rfilter[0].can_id = messageID;
+		rfilter[0].can_mask = messageIDMask | 0x80000000;
+		setsockopt(*sessionHandle, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter));
+		bind(*sessionHandle, (struct sockaddr *)&addr, sizeof(addr));
 	}
 
 	void FRC_NetworkCommunication_CANSessionMux_closeStreamSession(uint32_t sessionHandle) {
+		close(sessionHandle);
 	}
 
+	//messagesToRead: seems to do the same thing as maxMessages
+	//messagesRead: number of messages returned
 	void FRC_NetworkCommunication_CANSessionMux_readStreamSession(uint32_t sessionHandle, struct tCANStreamMessage *messages, uint32_t messagesToRead, uint32_t *messagesRead, int32_t *status) {
-		uint8_t data[8];
-		uint32_t timeStamp = 0;
-		int32_t mystatus = 0;
-		uint8_t tmpdata[8];
-		uint8_t dataSize;
-		FRC_NetworkCommunication_CANSessionMux_receiveMessage(&_messageID, 0, &messages->data[0], &dataSize, &timeStamp, &mystatus);
-		messages->messageID = _messageID;
-		*messagesRead = 1;
+		uint32_t q;
+		for(q = 0; q < messagesToRead; q++) {
+			struct can_frame msg;
+			int a = read(sessionHandle, &msg, sizeof(msg));
+			if(a == -1) break;
+
+			messages[*messagesRead].messageID = msg.can_id & ~0x80000000;
+			messages[*messagesRead].dataSize = msg.can_dlc;
+			memcpy(messages[*messagesRead].data, msg.data, 8);
+
+			struct timeval tv;
+			ioctl(s, SIOCGSTAMP, &tv);
+			if (last_tv.tv_sec == 0)   /* first init */
+				last_tv = tv;
+
+			messages[*messagesRead].timeStamp = timediffms(tv, last_tv); //diff.tv_sec*1000 + diff.tv_usec/1000;
+		}
+		*messagesRead = q;
 	}
 
 	//void FRC_NetworkCommunication_CANSessionMux_getCANStatus(float *percentBusUtilization, uint32_t *busOffCount, uint32_t *txFullCount, uint32_t *receiveErrorCount, uint32_t *transmitErrorCount, int32_t *status);
